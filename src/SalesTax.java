b867b15f19b0211c9f7ebbcaa31102548ea17370
giahuytruong <truonggh@mail.uc.edu>
2022-09-18 21:09:05 -0400

    Initial commit

---
+++ src/SalesTax.java
@@ -0,0 +1,11 @@
+public class SalesTax {
+
+    public static void  main(String[] args){
+        double salesTax = 0;
+        double price = 703;
+
+        salesTax = price * .05;
+        System.out.println("The price is " + price);
+        System.out.println("The sales tax is " + salesTax);
+    }
+}
